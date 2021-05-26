e34f694900aa88fe21eb0371892e3604589d2265
mhope <mhope@npontu.com>
2021-05-26 20:38:03 +0000

    feat(quasar): set up quasar to replace vuejs

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="flex flex-center">
+    <img alt="Quasar logo" src="../assets/logo.png">
+    <h1>Helllo</h1>
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
