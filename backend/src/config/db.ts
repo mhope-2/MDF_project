eff0cb8c65cb59cf14d3967fbf011ed783c3fbd5
mhope <mhope@npontu.com>
2021-05-28 20:28:01 +0000

    feat(search): added naive search feature

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -6,6 +6,7 @@ import { Sequelize } from "sequelize"
 //   dialect: 'mysql'/* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
 // })
 
+// MySQL DB coonfig -- DB_NAME, DB_USER, DB_PASSWORD, DB_HOST
 const sequelize = new Sequelize(String('mariadolldb'), String('mhope'), String('NpontuTestPassword'), {
   host: String('localhost'),
   dialect: 'mysql'/* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
